1fb8c05e5588b3d6ff30dd3af251f149c64c0fde
ziwankiw <zenoniwankiw2018@u.northwestern.edu>
2017-06-01 22:23:21 -0500

    Drive the robot straight

--- HW20/TechCup_Code/firmware/src/app.c
+++ HW20/TechCup_Code/firmware/src/app.c
@@ -330,6 +330,28 @@ void APP_Initialize(void) {
 
     /* Set up the read buffer */
     appData.readBuffer = &readBuffer[0];
+    
+    // MOTOR INITIALIZATION
+    RPA0Rbits.RPA0R = 0b0101; // A0 is OC1
+    TRISAbits.TRISA1 = 0;
+    LATAbits.LATA1 = 0; // A1 is the direction pin to go along with OC1
+
+    RPB2Rbits.RPB2R = 0b0101; // B2 is OC4
+    TRISBbits.TRISB3 = 0;
+    LATBbits.LATB3 = 0; // B3 is the direction pin to go along with OC4
+    
+     T2CONbits.TCKPS = 2; // prescaler N=4 
+    PR2 = 1200 - 1; // 10kHz
+    TMR2 = 0;
+    OC1CONbits.OCM = 0b110; // PWM mode without fault pin; other OC1CON bits are defaults
+    OC4CONbits.OCM = 0b110;
+    OC1RS = 0; // max allowed value is 1119
+    OC1R = 0; // read-only initial value
+    OC4RS = 0; // max allowed value is 1119
+    OC4R = 0; // read-only initial value
+    T2CONbits.ON = 1;
+    OC1CONbits.ON = 1;
+    OC4CONbits.ON = 1;
 
     startTime = _CP0_GET_COUNT();
 }
@@ -377,6 +399,12 @@ void APP_Tasks(void) {
             if (APP_StateReset()) {
                 break;
             }
+            
+            // SET MOTOR DIRECTION AND SPEED
+             LATAbits.LATA1 = 0; // direction
+                OC1RS = 600; // velocity, 50%
+                LATBbits.LATB3 = 0; // direction
+                OC4RS = 600; // velocity, 50%
 
             /* If a read is complete, then schedule a read
              * else wait for the current read to complete */
